5828ef07d1a3d4bc84e79352710520427b8e4afb
Matheus <matheusbrito174@gmail.com>
2021-01-02 18:47:14 -0300

    Incluindo chart.js

--- src/app/components/relatorio-display/relatorio-display.component.ts
+++ src/app/components/relatorio-display/relatorio-display.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Chart } from 'node_modules/chart.js/dist/Chart.js'
 
 @Component({
   selector: 'app-relatorio-display',
   templateUrl: './relatorio-display.component.html',
   styleUrls: ['./relatorio-display.component.css']
 })
-export class RelatorioDisplayComponent implements OnInit {
+export class RelatorioDisplayComponent implements OnInit, AfterViewInit {
   @Input() formData;
+
+  @ViewChild('tagsDistintasGraf',{static:true}) tagsDistintasGraf:ElementRef;
+
   fileText;
   legendas=[];
   tags=[];
@@ -17,6 +21,10 @@ export class RelatorioDisplayComponent implements OnInit {
 
   ngOnInit(){
     this.processarTexto();
+  }
+
+  ngAfterViewInit(){
+    this.plotarTagsDistintas();
   }
 
   ngOnChanges(changes){
@@ -141,5 +149,46 @@ export class RelatorioDisplayComponent implements OnInit {
     }
 
     return relacoes;
+  }
+
+  plotarTagsDistintas(){
+    let ctx = this.tagsDistintasGraf.nativeElement.getContext('2d');
+    console.log('elemento:'+ctx)
+    let tagsDistintasGrafico = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
   }
 }
